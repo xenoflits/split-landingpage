c6028fb1343a3ea3a04adc35efcab8bc5f889f61
unknown <soeradj.mahabiersing@gmail.com>
2018-02-19 19:21:11 +0100

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# split-landingpage" 
+
+A landing page with two sides. In this example, the left side is the designer side, and the right side a developer.
+
+There are two images that are used for the background on either side.
+Using css variables the colors are saved at the top of the css file. This way that can be changed more easy.
+
+On hover of either side, and ease-in-out transition effect is applied with a speed variable.
--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
+
+/*events*/
+left.addEventListener('mouseenter', () =>{
+    container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () =>{
+    container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right')
+});
+
+right.addEventListener('mouseleave', () =>{
+    container.classList.remove('hover-right');
+});
